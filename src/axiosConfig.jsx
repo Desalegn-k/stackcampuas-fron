239b0291892a25bd0883c43fdcd00e1c8f491f8a
Desalegn Kerie <desu2464@gmail.com>
2025-11-05 13:47:28 -0500

    server correction

--- src/axiosConfig.jsx
+++ src/axiosConfig.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5300/api",
+  baseURL: "https://stackcampus-desu.onrender.com",
 });
 export default axiosBase
